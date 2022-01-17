1d57cb63ea8399d650211dbb2c9c429e839b5e49
FoxSmoze <malfattothomas@gmail.com>
2022-01-17 09:14:46 +0100

    mise a jour

---
+++ exo1.py
@@ -0,0 +1,21 @@
+class Robot():
+    __name = "<unnamed>"
+    __power = False
+    __current_speed = 0
+    __battery_level = 0
+    __states = ['shutown', 'running']
+    
+
+
+    def __init__(self, name):
+        self.__name = name
+        print("Votre nom ?", format(self.__name))
+
+
+    def eteint 
+
+
+
+    """
+      Give your best code here ( •̀ ω •́ )✧
+    """
