71ed6f84b98bcd562f28056fa823e774aab2778f
FoxSmoze <malfattothomas@gmail.com>
2022-01-21 16:13:06 +0100

    Mise a jour estomac

--- human.py
+++ human.py
@@ -1,21 +1,38 @@
+import time
+
+
 class Human():
     __sexe = '<unnamed>'
+    __estomac = 0
 
 
-#Constructor
-    def __init__ (self, sexe=None):
+    #Constructor
+    def __init__ (self, sexe):
         if sexe:
             self.__sexe = sexe
 
 
-#Destructor
+    #Destructor
     def __del__(self):
         print("Auto destruction NOW")
     
-#Manger
+    #Manger
     def manger(self,aliments):
-        for i in aliments:
-            print("Vous avez mangé: %s aliments", aliments[i])
+        if type(aliments)==str:
+            print("Vous avez mangé:", aliments)
+            time.sleep(1)
+            self.__estomac = self.__estomac + 1
+        else:
 
+            for i in aliments:
+                print("Vous avez mangé: %s" %i)
+                time.sleep(1)
+                self.__estomac = self.__estomac + 1
 
-#
+    @property
+    def sexe(self):
+        return self.__sexe
+
+    @property
+    def estomac(self):
+        return self.__estomac
