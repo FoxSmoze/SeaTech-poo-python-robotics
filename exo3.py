e7c3212ceb299c1809226318afffc47a78bed84f
FoxSmoze <malfattothomas@gmail.com>
2022-01-24 11:59:40 +0100

    Creation class

---
+++ exo3.py
@@ -0,0 +1,38 @@
+from abc import ABCMeta, abstractmethod
+
+class Véhicule():
+    def __init__(self):
+        print("C'est un véhicule")
+        
+    pass
+
+class Voiture(Véhicule):
+    def __init__(self):
+        print("C'est une voiture")
+
+    pass
+
+class Moto(Véhicule):
+    def __init__(self):
+        print("C'est une moto et ouais bg")
+
+    pass
+
+class Scootbite(Véhicule):
+    def __init__(self):
+        print("C'est une merde de scoot")
+
+    pass
+
+class Compétition(Véhicule):
+    def __init__(self):
+        print("Ah ! La c'est un véhicule de compet frérot")
+
+    pass
+
+class WRC(Voiture, Compétition)
+    def __init__(self):
+        print("Et ouais c'est Loeb qui a gagné le rallye de mont'carl")
+        super().__init__()
+        
+    pass
